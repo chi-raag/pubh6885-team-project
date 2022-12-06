dfac8b9530300ab040cb73f4b2b1ae7618cd687f
Chiraag <37530396+chi-raag@users.noreply.github.com>
2022-12-06 12:30:21 -0500

    changed to shared drive

--- R/01_loading-data.R
+++ R/01_loading-data.R
@@ -3,11 +3,15 @@ library(SeuratDisk)
 library(googledrive)
 
 # List files in the shared directory
-drive_ls("pubh6885-tp-data")
+sd <- shared_drive_find() %>%
+  pull(name) %>%
+  shared_drive_get()
+drive_ls(sd)
 
 # Download expression matrix, convert into a dataframe, remove the local text
 # file, and then convert into a Seurat object
-drive_download("pubh6885-tp-data/rawcounts.txt.gz")
+
+drive_download(as_id("10FXWE5woGZhk8qKIXVwgw4tTjjM24FIC"))
 data <- read.table("rawcounts.txt.gz", sep = "\t", header = T)
 file.remove("rawcounts.txt.gz")
 obj <- CreateSeuratObject(data)
@@ -16,12 +20,14 @@ obj <- CreateSeuratObject(data)
 # column due to it being an index variable, and add the metadata to the Seurat
 # object
 metadata <- drive_read_string("~/pubh6885-tp-data/metadata.csv") %>%
-  read.csv(text = .)
+  read.csv(text = ., row.names = 2)
 metadata$X <- NULL
 obj <- obj |>
   AddMetaData(metadata)
 
 obj |> SaveH5Seurat("01_initial-object")
-drive_upload("01_initial-object.h5seurat",
-             "pubh6885-tp-data/01_initial-object.h5seurat")
+drive_upload("01_initial-object.h5seurat", sd)
 file.remove("01_initial-object.h5seurat")
+
+obj |>
+  VlnPlot(features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
