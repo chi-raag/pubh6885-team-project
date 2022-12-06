89dbeb232fcf7253afdecd483068a1cfd6c0e541
Chiraag <37530396+chi-raag@users.noreply.github.com>
2022-12-06 11:16:43 -0500

    added comments

--- R/01_loading-data.R
+++ R/01_loading-data.R
@@ -2,18 +2,26 @@ library(Seurat)
 library(SeuratDisk)
 library(googledrive)
 
+# List files in the shared directory
 drive_ls("pubh6885-tp-data")
 
-data <- drive_read_string("~/pubh6885-tp-data/rawcounts.txt") %>%
-  read.table(text = ., sep = "\t", header = T)
-
+# Download expression matrix, convert into a dataframe, remove the local text
+# file, and then convert into a Seurat object
+drive_download("pubh6885-tp-data/rawcounts.txt.gz")
+data <- read.table("rawcounts.txt.gz", sep = "\t", header = T)
+file.remove("rawcounts.txt.gz")
 obj <- CreateSeuratObject(data)
 
+# Read metadata from shared drive, read into a data frame, remove the first
+# column due to it being an index variable, and add the metadata to the Seurat
+# object
 metadata <- drive_read_string("~/pubh6885-tp-data/metadata.csv") %>%
-  read.csv(text = ., row.names = 2)
+  read.csv(text = .)
 metadata$X <- NULL
-
 obj <- obj |>
   AddMetaData(metadata)
 
-obj |> SaveH5Seurat("objects/01_initial-object")
+obj |> SaveH5Seurat("01_initial-object")
+drive_upload("01_initial-object.h5seurat",
+             "pubh6885-tp-data/01_initial-object.h5seurat")
+file.remove("01_initial-object.h5seurat")
