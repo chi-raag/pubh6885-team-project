6e68cf9728009d62a5f703f8cd221bca75ac998e
Chiraag <37530396+chi-raag@users.noreply.github.com>
2022-12-06 13:47:22 -0500

    finished data cleaning, updated renv

--- R/01_loading-data.R
+++ R/01_loading-data.R
@@ -30,4 +30,80 @@ drive_upload("01_initial-object.h5seurat", sd)
 file.remove("01_initial-object.h5seurat")
 
 obj |>
-  VlnPlot(features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+  VlnPlot(features = c("nFeature_RNA", "nCount_RNA", "Percent_Mitochondrial"), ncol = 3)
+plot1 <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "Percent_Mitochondrial")
+plot2 <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+
+obj <- obj |>
+  subset(nFeature_RNA > 200 &
+           nFeature_RNA < 7500 &
+           Percent_Mitochondrial < 5)
+
+obj |>
+  VlnPlot(features = c("nFeature_RNA", "nCount_RNA", "Percent_Mitochondrial"), ncol = 3)
+plot1 <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "Percent_Mitochondrial")
+plot2 <- FeatureScatter(obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+
+obj |>
+  SaveH5Seurat("02_post-qc")
+drive_upload("02_post-qc.h5seurat", sd)
+file.remove("02_post-qc.h5seurat")
+
+obj <- obj |>
+  SCTransform(vst.flavor = "v2")
+
+obj <- obj |>
+  RunPCA(assay = "SCT")
+
+obj |>
+  SaveH5Seurat("03_post-norm-pca")
+drive_upload("03_post-norm-pca.h5seurat", sd)
+file.remove("03_post-norm-pca.h5seurat")
+
+donor_agg_raw <- obj |> AggregateExpression(assay = "SCT",
+                                            features = VariableFeatures(obj),
+                                            group.by = "donor_id",
+                                            slot = "count")
+
+donor_agg_norm <- obj |> AggregateExpression(assay = "SCT",
+                                             features = VariableFeatures(obj),
+                                             group.by = "donor_id",
+                                             slot = "data")
+
+dist_matrix_raw <- donor_agg_raw$SCT |>
+  t() |>
+  dist()
+
+dist_matrix_norm <- donor_agg_norm$SCT |>
+  t() |>
+  dist()
+
+dist_matrix_raw %>%
+  as.matrix() %>%
+  write.table("dist_matrix_raw.tsv", sep = "\t")
+dist_matrix_norm %>%
+  as.matrix() %>%
+  write.table("dist_matrix_norm.tsv", sep = "\t")
+
+drive_upload("dist_matrix_norm.tsv", path = sd)
+drive_upload("dist_matrix_raw.tsv", path = sd)
+
+file.remove(c("dist_matrix_norm.tsv", "dist_matrix_raw.tsv"))
+
+obj_subset <- obj %>%
+  subset(features = VariableFeatures(obj))
+
+obj_subset@assays$RNA@counts %>%
+  as.matrix() %>%
+  write.table("raw_counts_subset.tsv", sep = "\t")
+
+drive_upload("raw_counts_subset.tsv", path = sd)
+file.remove("raw_counts_subset.tsv")
+
+obj_subset@meta.data |>
+  write.table("metadata_subset.tsv", sep = "\t")
+
+drive_upload("metadata_subset.tsv", path = sd)
+file.remove("metadata_subset.tsv")
