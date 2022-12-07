c7961ad5acca2017d36c5e117cb109c08f4d4314
Chiraag <37530396+chi-raag@users.noreply.github.com>
2022-12-06 21:40:51 -0500

    reorg

--- R/tweedie/seurat_tweedieverse.R
+++ R/tweedie/seurat_tweedieverse.R
@@ -1,12 +1,13 @@
 library(dplyr)
 library(Seurat)
 library(SeuratDisk)
+library(omicsArt)
 
 obj <-
   LoadH5Seurat("~/Google Drive/Shared drives/Pubh6885-team-project/03_post-norm-pca.h5seurat")
 
 obj_sub <- obj |>
-  subset(features = VariableFeatures(obj))
+  subset(features = VariableFeatures(obj)[1:2000])
 
 counts <- obj_sub@assays$RNA@counts |>
   as.matrix()
@@ -25,5 +26,7 @@ Tweedieverse(
   output = "output/tweedieverse",
   fixed_effects = c("groups"),
   base_model = 'CPLM',
-  adjust_offset = TRUE
+  adjust_offset = TRUE,
+  reference = "normal",
+  cores = 2
 )
